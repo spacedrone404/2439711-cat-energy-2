a0ff851ce07fd393fa74d108bcf20f6f0874ec85
spacedrone404 <81930707+spacedrone404@users.noreply.github.com>
2025-04-14 23:19:11 +0300

    Fixed by memory #1

--- source/scripts/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('./includes/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('.footer-container').innerHTML = data;
-        });
-});
