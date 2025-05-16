1fbaf3b0bd065327a1d639ec617f8c9f97cd235c
spacedrone404 <81930707+spacedrone404@users.noreply.github.com>
2025-05-16 17:27:59 +0300

    Submit

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -17,14 +17,3 @@ document.getElementById("burger")?.addEventListener("click", function () {
     }
 });
 
-// Old method
-// document.querySelector(".header").classList.remove("header--nojs");
-// document.querySelector(".menu-nav").classList.remove("menu-nav--nojs");
-// document
-//     .querySelector(".contacts__maps")
-//     .classList.remove("contacts__maps--nojs");
-
-// document.getElementById("burger").addEventListener("click", () => {
-//     document.querySelector(".menu-nav__list").classList.toggle("active");
-//     document.querySelector(".menu-nav__toggle").classList.toggle("active");
-// });
